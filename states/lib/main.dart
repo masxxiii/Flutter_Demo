73040c9b76aee5e562b92699b616a0d421905523
masxxiii <masxafar@gmail.com>
2022-05-15 16:05:11 +0700

    complete main

--- states/lib/main.dart
+++ states/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:states/screens/main_screen_provider.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(),
+    const MaterialApp(
+      home: Scaffold(
+          body: MainScreenProvider()
+      ),
     ),
   );
 }

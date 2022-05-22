a26ebd810971571234a452691eae02047fd0e03f
masxxiii <masxafar@gmail.com>
2022-05-22 16:11:11 +0700

    remove folder

--- data_persistence/android/app/src/main/kotlin/com/example/data_persistence/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.data_persistence
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- data_persistence/lib/screens/cubit/main_screen_state.dart
+++
@@ -1,9 +0,0 @@
-abstract class MainScreenState{}
-
-class MainScreenInitSumSquareCalculationState extends MainScreenState{}
-
-class MainScreenUpdateSumSquareCalculationState extends MainScreenState {
-  final num value;
-
-  MainScreenUpdateSumSquareCalculationState({required this.value});
-}

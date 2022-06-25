4625e4196dd27fd4c914d0fff4fdae2d8ce84bb7
Masood <57627236+masxxiii@users.noreply.github.com>
2022-06-25 23:16:07 +0700

    Update main_screen_state.dart

--- states/lib/screens/cubit/main_screen_state.dart
+++ states/lib/screens/cubit/main_screen_state.dart
@@ -3,7 +3,8 @@ abstract class MainScreenState{}
 class MainScreenInitSumSquareCalculationState extends MainScreenState{}
 
 class MainScreenUpdateSumSquareCalculationState extends MainScreenState {
-  final num value;
+  //variable to store final result
+final num value;
 
   MainScreenUpdateSumSquareCalculationState({required this.value});
 }

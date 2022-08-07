07339d01c782d65117e0b62669abe27bc1493fe9
Masood <57627236+masxxiii@users.noreply.github.com>
2022-08-07 20:27:42 +0700

    Update api.dart

--- api/lib/requests/api.dart
+++ api/lib/requests/api.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 Future<Map<String, dynamic>> getNasaData() async {
   Uri url = Uri.parse("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=50&api_key=eQnprvXukgfNomTanZiHT1DqLApcABzFjI350dyZ");
+
   final response = await http.get(url);
 
   if(response.statusCode == 200){
